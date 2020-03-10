f31cdf87c056972c14f50aec550c37627408ef01
jawad <jawad@digitify.com>
2020-03-10 14:44:18 +0500

    travis

--- ProgressImageViewTests/ProgressImageViewTests.swift
+++ ProgressImageViewTests/ProgressImageViewTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import ProgressImageView
 
 class ProgressImageViewTests: XCTestCase {
-
+//
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
