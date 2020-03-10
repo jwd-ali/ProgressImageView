3bf454f4f40dc40f14d0c5965f14275e753fa8b5
jawad <jawad@digitify.com>
2020-03-10 16:29:38 +0500

    carthage

---
+++ Carthage/Build/iOS/ProgressImageView.framework/Headers/ProgressImageView.h
@@ -0,0 +1,19 @@
+//
+//  ProgressImageView.h
+//  ProgressImageView
+//
+//  Created by Jawad Ali on 10/03/2020.
+//  Copyright © 2020 Jawad Ali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProgressImageView.
+FOUNDATION_EXPORT double ProgressImageViewVersionNumber;
+
+//! Project version string for ProgressImageView.
+FOUNDATION_EXPORT const unsigned char ProgressImageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressImageView/PublicHeader.h>
+
+
