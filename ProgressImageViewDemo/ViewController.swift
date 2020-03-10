b48fece112fb8ea95d21c6f11c650cb02ec16874
jawad <jawad@digitify.com>
2020-03-10 16:12:21 +0500

    Changes Committed

--- ProgressImageViewDemo/ViewController.swift
+++ ProgressImageViewDemo/ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 import ProgressImageView
 class ViewController: UIViewController {
-
+let progress = ProgessImageView(with: UIImage(named: "cup"), color: #colorLiteral(red: 0.4433879256, green: 0.2915558219, blue: 0.8948832154, alpha: 1), progress: 0 , animation: true)
     override func viewDidLoad() {
-        let progress = ProgessImageView(with: UIImage(named: "cup"), color: #colorLiteral(red: 0.4433879256, green: 0.2915558219, blue: 0.8948832154, alpha: 1), progress: 50 , animation: true)
+        
         progress.frame = CGRect(x: 50, y: 50, width: 150, height: 150)
         progress.center = self.view.center
         progress.lineWidth = 3
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        progress.progress = 70
+    }
 }
 
